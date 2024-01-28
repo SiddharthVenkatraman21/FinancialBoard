bbf60f06de9b8fb41521e5af4e64c500a2236dc2
ana-jimenez <anajimenez@tamu.edu>
2024-01-28 05:04:23 -0600

    able to register users appear in firebase

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useHistory } from "react-router-dom";
+import { db } from './firebase'; 
 import {
   auth,
   registerWithEmailAndPassword,
@@ -14,8 +15,35 @@ function Register() {
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
   const register = () => {
-    if (!name) alert("Please enter name");
-    registerWithEmailAndPassword(name, email, password);
+    if (!name) {
+      alert("Please enter name");
+      return;
+    }
+    
+    registerWithEmailAndPassword(name, email, password)
+      .then((userCredential) => {
+        // Registered successfully
+        const user = userCredential.user;
+        // Add a new document in collection "users" with UID as the document ID
+        db.collection("users").doc(user.uid).set({
+          name: name,
+          email: email,
+          stocks_of_interest: [],
+        })
+        .then(() => {
+          console.log("User profile created!");
+          navigate("/dashboard", { replace: true });
+        })
+        .catch((error) => {
+          console.error("Error writing document: ", error);
+        });
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ...
+      });
   };
   useEffect(() => {
     if (loading) return;
