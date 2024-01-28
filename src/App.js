8dbc46e894da21472a072f41d4fbc132488eba92
Joshua Robert <joshuarobert@client-10-229-139-231.tamulink.tamu.edu>
2024-01-28 03:31:41 -0600

    budgetCalculatorWGraph

--- src/App.js
+++ src/App.js
@@ -6,23 +6,39 @@ import BudgetCalculator from './components/budgetCalculator';
 import NewsComponent from './components/stockNewsCard';
 import CompanyNewsContainer from './components/CompanyNewsContainer'
 import StockNewsContainer from './components/stockNewsContainer'
-
-
+import BudgetGraph from './components/budgetGraph'; 
 
 function App() {
+  const [budgetData, setBudgetData] = useState({
+    income: null,
+    expenses: {
+      needs: null,
+      wants: null,
+      savings: null,
+    },
+    idealPercentages: {
+      necessities: 50,
+      wants: 30,
+      savings: 20,
+    },
+  });
 
-  
-
-    
+  const handleInputChange = (data) => {
+    // Update the parent state
+    setBudgetData((prevData) => ({
+      ...prevData,
+      ...data,
+    }));
+  };
 
   return (
     <div className="App">
-      
-      {/* <BudgetCalculator/>
-      <StockQuote/> */}
       <div className="StockBudget">
-        <StockQuote/>
-        <BudgetCalculator/>
+        {/* Pass budgetData and handleInputChange as props to BudgetCalculator */}
+        <StockQuote/> 
+        <BudgetCalculator budgetData={budgetData} onInputChange={handleInputChange} />
+        {/* Pass necessary data as props to BudgetGraph */}
+        <BudgetGraph income={budgetData.income} needs={budgetData.expenses.needs} wants={budgetData.expenses.wants} savings={budgetData.expenses.savings} />
       </div>
       <CompanyNewsContainer/>
       <StockNewsContainer/>
